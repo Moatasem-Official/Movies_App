c54fad33de702a4c66f2284c7960256566b9b067
Moatasem <moatasemnagy3@gmail.com>
2025-08-17 00:50:11 +0300

    Make Now Playing Movies Entity As Equatable

--- lib/Movies/features/now_playing_movies/domain/entities/now_playing_movies_entity.dart
+++ lib/Movies/features/now_playing_movies/domain/entities/now_playing_movies_entity.dart
@@ -1,18 +1,26 @@
-class NowplayingMoviesEntity {
+import 'package:equatable/equatable.dart';
+
+class NowplayingMoviesEntity extends Equatable {
   final DatesEntity dates;
   final List<ResultEntity> results;
 
   const NowplayingMoviesEntity({required this.dates, required this.results});
+
+  @override
+  List<Object?> get props => [dates, results];
 }
 
-class DatesEntity {
+class DatesEntity extends Equatable {
   final String maximum;
   final String minimum;
 
   const DatesEntity({required this.maximum, required this.minimum});
+
+  @override
+  List<Object?> get props => [maximum, minimum];
 }
 
-class ResultEntity {
+class ResultEntity extends Equatable {
   final String backdropPath;
   final List<int> genreIds;
   final int id;
@@ -40,4 +48,20 @@ class ResultEntity {
     required this.voteAverage,
     required this.voteCount,
   });
+
+  @override
+  List<Object?> get props => [
+    backdropPath,
+    genreIds,
+    id,
+    originalLanguage,
+    originalTitle,
+    overview,
+    popularity,
+    posterPath,
+    releaseDate,
+    title,
+    voteAverage,
+    voteCount,
+  ];
 }
