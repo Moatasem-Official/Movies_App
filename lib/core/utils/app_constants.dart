6cfc298a4fede8c1ce85d520020998441f0556cd
Moatasem <moatasemnagy3@gmail.com>
2025-08-16 03:47:33 +0300

    Add App Constants Class

---
+++ lib/core/utils/app_constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const String kReadAccessToken =
+      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYzBjNzc0NGRiNDM1ZDU5MWQ5NzZlNjQyMmE5ZWY4ZSIsIm5iZiI6MTc1NTMwMzkxMC45ODg5OTk4LCJzdWIiOiI2ODlmY2ZlNjc2MTJkMDExYjg4NWI5MTMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.36jw4ieqQj_9dXU5zXsa6KgfzZiMJ3ukQ0NUqCjuPQw';
+  static const String kApiKey = '0c0c7744db435d591d976e6422a9ef8e';
+  static const String kApiBaseUrl = 'https://api.themoviedb.org/3/';
+}
