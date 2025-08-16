f4b530029142d12d1d2e592439bcf28bb1b4600f
Moatasem <moatasemnagy3@gmail.com>
2025-08-16 15:44:35 +0300

    Setup Now Playing Movies Feature Use Case & Implement Data Repo

--- lib/Movies/features/now_playing_movies/data/repository/now_playing_movies_data_repo.dart
+++ lib/Movies/features/now_playing_movies/data/repository/now_playing_movies_data_repo.dart
@@ -1,7 +1,13 @@
+import 'package:movies_app/Movies/features/now_playing_movies/data/datasource/remote_data_source.dart';
 import 'package:movies_app/Movies/features/now_playing_movies/domain/entities/now_playing_movies_entity.dart';
 import 'package:movies_app/Movies/features/now_playing_movies/domain/repository/now_playing_movies_domain_repo.dart';
 
 class NowPlayingMoviesDataRepo implements NowPlayingMoviesDomainRepo {
+  final RemoteDataSource remoteDataSource;
+  NowPlayingMoviesDataRepo(this.remoteDataSource);
+
   @override
-  Future<NowplayingMoviesEntity> getNowPlayingMovies() async {}
+  Future<NowplayingMoviesEntity> getNowPlayingMovies() async {
+    return await remoteDataSource.getNowPlayingMovies();
+  }
 }
