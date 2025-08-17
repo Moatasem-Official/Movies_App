0abe5f3c2e9377c12e3525c6c1576b9b1ff7bb60
Moatasem <moatasemnagy3@gmail.com>
2025-08-18 02:07:06 +0300

    Add Slider To Home Screen

--- lib/Movies/features/display_different_movies_types/presentation/screens/movies_home_screen.dart
+++ lib/Movies/features/display_different_movies_types/presentation/screens/movies_home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class MoviesHomeScreen extends StatelessWidget {
@@ -5,6 +6,39 @@ class MoviesHomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Movies Home Screen')));
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CarouselSlider.builder(
+              itemCount: 15,
+              itemBuilder:
+                  (BuildContext context, int itemIndex, int pageViewIndex) =>
+                      Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 123, 7, 255),
+                        ),
+                        child: Center(child: Text(itemIndex.toString())),
+                      ),
+              options: CarouselOptions(
+                height: 350,
+                aspectRatio: 16 / 9,
+                viewportFraction: 1,
+                initialPage: 0,
+                enableInfiniteScroll: true,
+                reverse: false,
+                autoPlay: true,
+                autoPlayInterval: const Duration(seconds: 3),
+                autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enlargeCenterPage: false,
+                scrollDirection: Axis.horizontal,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
