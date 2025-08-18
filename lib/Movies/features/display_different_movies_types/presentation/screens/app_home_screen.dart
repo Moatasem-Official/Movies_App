307bcd15fd9edca1b92030282f71ff3a8b305f85
Moatasem <moatasemnagy3@gmail.com>
2025-08-18 06:18:16 +0300

    Change Bottom Nav Bar To Better Design

--- lib/Movies/features/display_different_movies_types/presentation/screens/app_home_screen.dart
+++ lib/Movies/features/display_different_movies_types/presentation/screens/app_home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:movies_app/Movies/features/display_different_movies_types/presentation/screens/movies_home_screen.dart';
-import 'package:water_drop_nav_bar/water_drop_nav_bar.dart';
+import 'package:responsive_navigation_bar/responsive_navigation_bar.dart';
 
 class AppHomeScreen extends StatefulWidget {
   const AppHomeScreen({super.key});
@@ -11,39 +11,58 @@ class AppHomeScreen extends StatefulWidget {
 }
 
 class _AppHomeScreenState extends State<AppHomeScreen> {
-  int selectedIndex = 0;
   PageController pageController = PageController();
   List<Widget> screens = [
     const MoviesHomeScreen(),
     const Center(child: Text('TV Screen')),
   ];
+  int _selectedIndex = 0;
+
+  void changeTab(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    pageController.jumpToPage(index);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        physics: NeverScrollableScrollPhysics(),
+      extendBody: true,
+      body: PageView.builder(
+        scrollDirection: Axis.horizontal,
         controller: pageController,
-        children: screens,
+        itemCount: screens.length,
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) => screens[index],
       ),
-      bottomNavigationBar: WaterDropNavBar(
-        inactiveIconColor: Colors.white,
-        waterDropColor: Colors.teal,
-        backgroundColor: Color(0xFF1E1E29),
-        onItemSelected: (index) {
-          setState(() {
-            selectedIndex = index;
-          });
-          pageController.animateToPage(
-            selectedIndex,
-            duration: const Duration(milliseconds: 400),
-            curve: Curves.easeOutQuad,
-          );
-        },
-        selectedIndex: selectedIndex,
-        barItems: [
-          BarItem(filledIcon: MdiIcons.movie, outlinedIcon: MdiIcons.movie),
-          BarItem(filledIcon: Icons.tv, outlinedIcon: Icons.tv_outlined),
+      bottomNavigationBar: ResponsiveNavigationBar(
+        selectedIndex: _selectedIndex,
+        onTabChange: changeTab,
+        textStyle: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        backgroundColor: Colors.black.withOpacity(0.85),
+        navigationBarButtons: <NavigationBarButton>[
+          NavigationBarButton(
+            text: 'Movies',
+            icon: MdiIcons.movie,
+            backgroundGradient: const LinearGradient(
+              colors: [Color(0xFF0f2027), Color(0xFF203a43), Color(0xFF2c5364)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          NavigationBarButton(
+            text: 'TVs',
+            icon: Icons.tv,
+            backgroundGradient: const LinearGradient(
+              colors: [Color(0xFF232526), Color(0xFF414345)],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+            ),
+          ),
         ],
       ),
     );
