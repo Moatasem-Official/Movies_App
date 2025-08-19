201b64b8a48ad770f9cfa4aff596a9860cb8332a
Moatasem <moatasemnagy3@gmail.com>
2025-08-20 02:02:45 +0300

    Initialize Cubit In App Router Class

--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_app/Movies/features/display_different_movies_types/presentation/controllers/movies_home_screen/cubit/movies_home_screen_cubit.dart';
 import 'package:movies_app/Movies/features/display_different_movies_types/presentation/screens/app_home_screen.dart';
 import 'package:movies_app/Movies/features/display_different_movies_types/presentation/screens/movie_details_screen.dart';
 import 'package:movies_app/Movies/features/display_different_movies_types/presentation/screens/movies_home_screen.dart';
 import 'package:movies_app/Movies/features/display_different_movies_types/presentation/screens/see_all_elements_list_screen.dart';
 import 'package:movies_app/Movies/features/display_different_movies_types/presentation/screens/splash_screen.dart';
+import 'package:movies_app/Movies/movies_injection.dart';
 
 class AppRouter {
   static const String splashScreen = '/';
@@ -18,7 +21,17 @@ class AppRouter {
       case splashScreen:
         return MaterialPageRoute(builder: (_) => SplashScreen());
       case appHomeScreen:
-        return MaterialPageRoute(builder: (_) => AppHomeScreen());
+        return MaterialPageRoute(
+          builder: (_) => MultiBlocProvider(
+            providers: [
+              BlocProvider<MoviesHomeScreenCubit>(
+                create: (_) =>
+                    getIt<MoviesHomeScreenCubit>()..getNowPlayingMovies(),
+              ),
+            ],
+            child: AppHomeScreen(),
+          ),
+        );
       case moviesHomeScreen:
         return MaterialPageRoute(builder: (_) => MoviesHomeScreen());
       case seeAllElementsListScreen:
