02e91e2a8e88f61963635fb6608c19aa9ee42afe
Moatasem <moatasemnagy3@gmail.com>
2025-08-20 00:04:10 +0300

    Add Cubit Get Now Playing Movies Function Implementation

--- lib/Movies/features/display_different_movies_types/presentation/controllers/movies_home_screen/cubit/movies_home_screen_cubit.dart
+++ lib/Movies/features/display_different_movies_types/presentation/controllers/movies_home_screen/cubit/movies_home_screen_cubit.dart
@@ -1,11 +1,21 @@
 import 'package:bloc/bloc.dart';
-import 'package:movies_app/Movies/features/display_different_movies_types/data/models/result_model.dart';
+import 'package:movies_app/Movies/features/display_different_movies_types/domain/entities/display_different_movies_types_entity.dart';
+import 'package:movies_app/Movies/features/display_different_movies_types/domain/usecases/get_now_playing_movies_use_case.dart';
 import 'package:movies_app/Movies/features/display_different_movies_types/presentation/controllers/Movies_Module_States/movies_module_states.dart';
+import 'package:movies_app/Movies/movies_injection.dart';
 
-class MoviesHomeScreenCubit extends Cubit<MoviesModuleStates<ResultModel>> {
+class MoviesHomeScreenCubit
+    extends Cubit<MoviesModuleStates<List<ResultEntity>>> {
   MoviesHomeScreenCubit() : super(const Idle());
 
-  Future<void> getNowPlayingMovies() async {}
+  Future<void> getNowPlayingMovies() async {
+    emit(const Loading());
+    final result = await getIt<GetNowPlayingMoviesUseCase>().call();
+    result.fold(
+      (left) => emit(Error(left.message)),
+      (right) => emit(Loaded(right.results)),
+    );
+  }
 
   Future<void> getPopularMovies() async {}
 
