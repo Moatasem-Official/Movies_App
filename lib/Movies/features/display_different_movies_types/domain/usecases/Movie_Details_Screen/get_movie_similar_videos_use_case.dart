6432a5f1afe70bed20119db463be897fc969471b
Moatasem <moatasemnagy3@gmail.com>
2025-08-22 00:04:51 +0300

    Setup Movie Videos UseCase

---
+++ lib/Movies/features/display_different_movies_types/domain/usecases/Movie_Details_Screen/get_movie_similar_videos_use_case.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:movies_app/Movies/features/display_different_movies_types/domain/entities/movie_videos_entity.dart';
+import 'package:movies_app/Movies/features/display_different_movies_types/domain/repository/different_movies_types_domain_repo.dart';
+import 'package:movies_app/core/error/failure.dart';
+
+class GetMovieSimilarVideosUseCase {
+  GetMovieSimilarVideosUseCase(this.differentMoviesTypesDomainRepo);
+  final DifferentMoviesTypesDomainRepo differentMoviesTypesDomainRepo;
+
+  Future<Either<Failure, MovieVideosEntity>> call({required int movieId}) =>
+      differentMoviesTypesDomainRepo.getMovieVideos(movieId: movieId);
+}
