81944b7150466e20ba1b37f958fe7ecff93de54a
Moatasem <moatasemnagy3@gmail.com>
2025-08-21 23:57:05 +0300

    Setup Movie Videos Models and Entities

---
+++ lib/Movies/features/display_different_movies_types/data/models/movie_videos_model.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'movie_videos_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MovieVideosModel _$MovieVideosModelFromJson(Map<String, dynamic> json) =>
+    MovieVideosModel(
+      id: (json['id'] as num).toInt(),
+      results: (json['results'] as List<dynamic>)
+          .map((e) => ResultVideoModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$MovieVideosModelToJson(MovieVideosModel instance) =>
+    <String, dynamic>{'id': instance.id, 'results': instance.results};
---
+++ lib/Movies/features/display_different_movies_types/domain/entities/movie_videos_entity.dart
@@ -0,0 +1,32 @@
+class MovieVideosEntity {
+  final int id;
+  final List<ResultVideoEntity> results;
+
+  const MovieVideosEntity({required this.id, required this.results});
+}
+
+class ResultVideoEntity {
+  final String iso_639_1;
+  final String iso_3166_1;
+  final String name;
+  final String key;
+  final String site;
+  final int size;
+  final String type;
+  final bool official;
+  final String publishedAt;
+  final String id;
+
+  const ResultVideoEntity({
+    required this.iso_639_1,
+    required this.iso_3166_1,
+    required this.name,
+    required this.key,
+    required this.site,
+    required this.size,
+    required this.type,
+    required this.official,
+    required this.publishedAt,
+    required this.id,
+  });
+}
