29d605c1082a9b8d6621d876b63fe53b37a0c982
Moatasem <moatasemnagy3@gmail.com>
2025-10-03 06:34:48 +0300

    Change Discover Movies Categories Container Color

--- lib/core/utils/app_constants.dart
+++ lib/core/utils/app_constants.dart
@@ -16,79 +16,79 @@ class AppConstants {
   static const Map<int, Map<String, dynamic>> genreDetails = {
     27: {
       'icon': Icons.nightlight_round,
-      'color': Color(0xFF0F2038),
+      'color': Color(0xFF070E18),
     },
     80: {
       'icon': Icons.fingerprint_rounded,
-      'color': Color(0xFF152A4A),
+      'color': Color(0xFF0A1520),
     },
     53: {
       'icon': Icons.bolt_rounded,
-      'color': Color(0xFF1A355A),
+      'color': Color(0xFF0D1A28),
     },
     10752: {
       'icon': Icons.shield_rounded,
-      'color': Color(0xFF1B2E4B),
+      'color': Color(0xFF101F30),
     },
     36: {
       'icon': Icons.account_balance_rounded,
-      'color': Color(0xFF233B5E),
+      'color': Color(0xFF132538),
     },
     99: {
       'icon': Icons.camera_roll_rounded,
-      'color': Color(0xFF35588F),
+      'color': Color(0xFF162A40),
     },
     18: {
       'icon': Icons.theater_comedy_rounded,
-      'color': Color(0xFF2A4C7A),
+      'color': Color(0xFF1A3049),
     },
     9648: {
       'icon': Icons.help_outline_rounded,
-      'color': Color(0xFF1E3A5F),
+      'color': Color(0xFF1D3651),
     },
     28: {
       'icon': Icons.local_fire_department_rounded,
-      'color': Color(0xFF3C619A),
+      'color': Color(0xFF213B59),
     },
     12: {
       'icon': Icons.explore_rounded,
-      'color': Color(0xFF4871B2),
+      'color': Color(0xFF244061),
     },
     878: {
       'icon': Icons.rocket_launch_rounded,
-      'color': Color(0xFF4269A6),
+      'color': Color(0xFF274569),
     },
     37: {
       'icon': Icons.wb_sunny_outlined,
-      'color': Color(0xFF305182),
+      'color': Color(0xFF2B4B71),
     },
     14: {
       'icon': Icons.castle_rounded,
-      'color': Color(0xFF25416B),
+      'color': Color(0xFF2E5079),
     },
     10751: {
       'icon': Icons.family_restroom_rounded,
-      'color': Color(0xFF5581CA),
+      'color': Color(0xFF325581),
     },
     16: {
       'icon': Icons.auto_awesome_rounded,
-      'color': Color(0xFF6291E2),
+      'color': Color(0xFF355B89),
     },
     35: {
       'icon': Icons.sentiment_very_satisfied_rounded,
-      'color': Color(0xFF70A1FA),
+      'color': Color(0xFF396091),
     },
     10402: {
       'icon': Icons.music_note_rounded,
-      'color': Color(0xFF5B89D6),
+      'color': Color(0xFF3C6699),
     },
     10749: {
       'icon': Icons.favorite_rounded,
-      'color': Color(0xFF6899EE),
+      'color': Color(0xFF406BA1),
     },
     10770: {
       'icon': Icons.tv_rounded,
-      'color': Color(0xFF4E79BE),
+      'color': Color(0xFF4371A9),
     },
   };
 }
